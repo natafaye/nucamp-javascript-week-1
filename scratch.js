122fa696a32d1f620629224709d3322636abca15
Natalie Childs <nata.faye@gmail.com>
2025-04-19 13:34:16 -0700

    go to bed

--- scratch.js
+++ scratch.js
@@ -1,82 +1,97 @@
-// alert('hi')
-// alert("goodbye")
-alert('I cant "do" this')
+//const myFavoriteFood = prompt("What's your favorite food?")
+// const is actually considered the default in JS
+// because it helps prevent bugs and it's more readable
 
-// shorthand
+// A const variable and a constant are not the same thing
+// const it won't change later
+// constant it will always have this exact value
 
-let number = 3
+// if(myFavoriteFood === "spaghetti") {
+//     alert("Your favorite is spaghetti")
+// }
 
-number++ // number = number + 1
-number += 2 // number = number + 2
+// One equals = is a command
+// Your favorite food is now spaghetti
 
+// Triple equals === is a question
+// Is your favorite food spaghetti?
 
-let isSweet = true
-let hasDairy = false
-let isCooked = true
-let isHot = false
+let count = 3
 
-isSweet && (hasDairy || isCooked && !isHot)
+count = 4
 
+count += 5 // count = count + 5
 
-// Single equals =
-// Statement
-// number = 3
-// The number IS three now
-// We are making it three
+let friend = "fish"
+friend = "hippo"
 
-// Triple equals ===
-// Question
-// number === 3
-// Is the number three?
-// This gives true if it is, and false if it is not
+friend += "fish" // friend = "hippo" + "fish"
 
 
+let dogName
+let likesFetch
+let sheds
+let pottyTrained
+let likesSnuggles
 
-let sunny = true
-let rainy = true
+// Milo
+dogName = "Milo"
+likesFetch = true
+sheds = true
+pottyTrained = true
+likesSnuggles = true
 
-if (sunny) {
-    // detour code
-    // maybe run
-    alert("Wear a hat")
-}
+// He wants it to be potty trained
+// He doesn’t want it to shed
+// He wants it to like to snuggle or like to play fetch (or both)
 
-let apple = 3
+// && || ! ()
+// dogName, likesFetch, sheds, pottyTrained, likesSnuggles
 
-// saving a contact in your phone
-// declaring the function
-function cookInOven() {
-    // saved code that we can trigger anytime we want
-}
+const rafaelShouldGetMilo = pottyTrained && !sheds && (likesFetch || likesSnuggles)
 
+console.log("Should I get Milo", rafaelShouldGetMilo)
+
+// Why parenthesis? Because && is greedy (or is higher precedence in the order of operations)
+// (3 + 4) * 5 <-- 35 or 23
+
+// Penny
+dogName = "Penny"
+likesFetch = false
+sheds = false
+pottyTrained = true
+likesSnuggles = true
+
+const rafaelShouldGetPenny = pottyTrained && !sheds && (likesFetch || likesSnuggles)
+
+console.log("Should I get Penny", rafaelShouldGetPenny)
+
+
+// Making the function (saving it) technical term is declaring it
 function putKidsToBed() {
-    alert("GO TO BED")
-    alert("GO TO BED")
-    alert("GO TO BED")
-    alert("GO TO BED")
-    alert("GO TO BED")
-    alert("GO TO BED")
+    // the code to run when this function is called
+    // alert("Go to bed!")
+    // alert("Go to bed!")
+    // alert("Go to bed!")
+    // alert("Go to bed!")
+    // alert("Go to bed!")
 }
 
-// trigger that function
-// calling the function
+// trigger / call / run
 putKidsToBed()
 putKidsToBed()
 
 
+// parameters are just variables that the function makes for you
 
-
-function cookInOven(food, temperature) { // let food = "rice"; let temperature = 200
+function cookInOven(food, temperature) { // let food = FIRST THING; let temperature = SECOND THING
     let cookedFood = "Cooked " + food + " at " + temperature
-    return cookedFood
+    return cookedFood // return always simplifies down to a value then returns that
 }
 
-// Another way to write basically the same thing
-const cookInOven = (food, temperature) => { // let food = "rice"; let temperature = 200
-    let cookedFood = "Cooked " + food + " at " + temperature
-    return cookedFood
-    alert("This code will never run")
-}
-
-let cookedRice = cookInOven("rice", 200) // WAIT
+// The way code runs it simplifies the right hand side of the equal sign to a value
+// a call to a function simplifies to whatever that function returns
+// parameters will simplify to a value then that value gets passed in
+let cookedRice = cookInOven("rice", 200)
+let cookedSomethingElse = cookInOven("chicken", 450)
 console.log(cookedRice)
